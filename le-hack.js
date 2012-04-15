b332afd6b0b6bdd462ab684f7d6b995548d84bc6
Liu Jialong <jialong08@gmail.com>
2012-04-16 06:18:46 +0800

    Clear Issue 1 partly.

--- le-hack.js
+++ le-hack.js
@@ -1,5 +1,24 @@
 // textfields of replies
 tfs = document.getElementsByClassName('maxlength');
-for (i in tfs) {
-    tfs[i].innerHTML = 500;                           // this is the hard limit imposed by *le server.
+
+// Called once on load. Called again when "Home" is clicked.
+function crackLengthLimit () {
+    for (i in tfs) {
+        tfs[i].innerHTML = 500;
+    }
 }
+
+spans = document.getElementsByTagName('span')
+homeButtonAnchor = null;
+for ( i=0; i<spans.length; i++ ) {
+    if (spans[i].attributes['stats'] != null &&
+        spans[i].attributes['stats'].textContent == 'V6Hd_home')
+        homeButtonAnchor = spans[i];
+}
+
+
+homeButtonAnchor.onclick = function() {
+    setTimeout('crackLengthLimit()', 3000);
+}
+
+crackLengthLimit();
